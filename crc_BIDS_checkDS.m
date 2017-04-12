ee1cf8f6c75bbd82b9faf1630baac7f786241d92
Christophe Phillips <c.phillips@ulg.ac.be>
2017-04-12 17:05:00 +0200

    Cosmetic fixes

--- crc_BIDS_checkDS.m
+++ crc_BIDS_checkDS.m
@@ -1,5 +1,12 @@
 function [BIDSall,lDir] = crc_BIDS_checkDS(rootDir)
 % Checking a series of BIDS-ds.
+% Use those provided as an example from the BIDS account:
+% https://github.com/INCF/BIDS-examples
+%_______________________________________________________________________
+% Copyright (C) 2017 Cyclotron Research Centre
+
+% Written by C. Phillips.
+% Cyclotron Research Centre, University of Liege, Belgium
 
 if nargin==0
     rootDir = pwd;
